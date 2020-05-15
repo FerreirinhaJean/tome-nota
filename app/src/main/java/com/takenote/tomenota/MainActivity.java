7d144e645ec47449400f1ad03ce996ab82cccf41
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-05-15 16:17:50 -0300

    Slider criado

--- app/src/main/java/com/takenote/tomenota/MainActivity.java
+++ app/src/main/java/com/takenote/tomenota/MainActivity.java
@@ -2,13 +2,48 @@ package com.takenote.tomenota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.takenote.tomenota.controller.PrincipalActivity;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.intro_start)
+                .canGoForward(false)
+                .build()
+        );
+
     }
+
+
+    public void btnComecar(View view) {
+        startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
+        finish();
+    }
+
 }
---
+++ app/src/main/java/com/takenote/tomenota/controller/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.takenote.tomenota.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.takenote.tomenota.MainActivity;
+import com.takenote.tomenota.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
